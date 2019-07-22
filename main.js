1b9b24703c82c784e9ef57f23e2a25f85aa8248b
rodriguesporan <rodriguesporan@gmail.com>
2019-07-22 13:36:44 -0300

    Adicionada imagem com referencia ao objeto data

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var app = new Vue({
     product: {
       name: 'Socks',
       description: 'Beautiful and colored socks',
+      image: './assets/vmSocks-green-onWhite.jpg',
+      link: 'http://127.0.0.1:5500/index.html',
     }
   }
 });
