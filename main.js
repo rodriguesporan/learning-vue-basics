51ba488899cf01e8423fb233bade5e9ffbe942ee
rodriguesporan <rodriguesporan@gmail.com>
2019-07-23 11:17:05 -0300

    Propriedades computadas

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: {
       name: 'Socks',
       description: 'Beautiful and colored socks',
       link: 'http://127.0.0.1:5500/index.html',
-      image: './assets/vmSocks-green-onWhite.jpg',
-      inStock: true,
+      selectedVariant: 0,
       details: ['80% cotton', '20% polyester', 'Gender neutral'],
       variants: [
         {
           variantId: 2234,
           variantColor: "green",
           variantImage: './assets/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10,
         },
         {
           variantId: 2235,
           variantColor: "blue",
           variantImage: './assets/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0,
         },
       ],
       sizes: ['34 - 35', '36 - 41', '42 - 44'],
@@ -28,11 +30,26 @@ var app = new Vue({
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.product.image = variantImage;
+    updateProduct(index) {
+      this.product.selectedVariant = index;
     },
     rmFromCart() {
       this.cart -= 1;
     }
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product.name}`;
+    },
+    image() {
+      const { product } = this;
+      const { variants } = product;
+      return variants[product.selectedVariant].variantImage;
+    },
+    inStock() {
+      const { product } = this;
+      const { variants } = product;
+      return variants[product.selectedVariant].variantQuantity > 0;
+    },
   }
 });
